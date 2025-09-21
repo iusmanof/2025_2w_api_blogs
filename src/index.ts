32baef195115fdf71074d4279a48c5543208fa4a
iusmanof <iusmanof@gmail.com>
2025-09-21 08:51:52 +0300

    simple express ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express, {Request, Response} from "express";
 
 const app = express()
-const port = process.env.port || 4000
+const port = process.env.port || 3000
 app.use(express.json());
 
-app.get('/', (req:Request, res: Response) => {
-    res.send('video api')
+app.get('/', (req: Request, res: Response) => {
+    res.send('blogs api')
   }
 )
 
 app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
 
 export default app;
